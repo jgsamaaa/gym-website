db9c760c5ac91946bdb65c66feb249e0e3de83c0
James Gabriel Dumaug <jamesgabrieldumaug@gmail.com>
2022-10-17 14:11:27 -0600

    workout section/slider done

--- src/components/WorkoutSlider.js
+++ src/components/WorkoutSlider.js
@@ -1,7 +1,58 @@
 import React from 'react';
 
+//import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+//import swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import '../workoutSlider.css';
+
+//import requred modules
+import { Navigation } from 'swiper';
+import { workouts } from '../data';
+
 const WorkoutSlider = () => {
-  return <div>WorkoutSlider</div>;
+  //destructure data
+  const { programs } = workouts;
+
+  return (
+    <Swiper
+      slidesPerView={2}
+      spaceBetween={32}
+      navigation={true}
+      //grabCursor={true}
+      breakpoints={{
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      }}
+      modules={[Navigation]}
+      className="workoutSlider"
+    >
+      {programs.map((program, idx) => {
+        // destructure program
+        const { image, name } = program;
+        return (
+          <SwiperSlide
+            className="max-w-[320px] max-h-[320px] relative bg-purple-200"
+            key={idx}
+          >
+            <img className="w-full h-full object-cover" src={image} alt="" />
+            <div className="absolute left-[20px] bottom-[20px] bg-white h-[26px] px-[14px] flex items-center justify-center rounded-[1px] cursor-pointer">
+              <div className="font-primary font-semibold text-sm text-neutral-500">
+                {name}
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default WorkoutSlider;
--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
+//impor data
+import { workouts } from '../data';
+
+//import components
+import WorkoutSlider from './WorkoutSlider';
+
 const Workouts = () => {
-  return <div>Workouts</div>;
+  //destructure workouts data
+  const { title, icon } = workouts;
+
+  return (
+    <section className="section">
+      {/* section title */}
+      <div
+        className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
+        <img src={icon} alt="" />
+        <h2 className="h2 section-title">
+          {title} <span className="text-primary-200">.</span>
+        </h2>
+      </div>
+      {/* Slider */}
+      <div data-aos="de-up" data-aos-delay="300">
+        <WorkoutSlider />
+      </div>
+    </section>
+  );
 };
 
 export default Workouts;
