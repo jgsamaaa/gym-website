042f2a22eeb80fa5a13daa9de4386c37f7ab4c52
James Gabriel Dumaug <jamesgabrieldumaug@gmail.com>
2022-10-17 18:42:21 -0600

    debugging

--- src/components/Join.js
+++ src/components/Join.js
@@ -5,12 +5,12 @@ import { join } from '../data';
 
 const Join = () => {
   // destructure join data
-  const { image, title, subtitle, btnText } = join;
+  const { image, subtitle, btnText } = join;
   return (
     <section className="bg-neutral-500 min-h-[537px]">
       <div className="container mx-auto">
         {/* image & text wrapper */}
-        <div className="flex flex-col md:flex-row md:items-center md:-space-x-12 -space-y-24 lg:-space-y-0">
+        <div className="flex flex-col -space-y-24 md:flex-row md:items-center md:-space-x-12 lg:-space-y-0">
           {/* image */}
           <div
             className="-mt-[80px] max-w-[276px] md:max-w-[442px] lg:max-w-full"
